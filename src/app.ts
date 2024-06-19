d940cd215d16b979c0c6e438053e13c4ae4d57e5
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-19 23:37:31 +0530

    Add: Error Middleware added

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import 'dotenv/config';
 import connectDB from './config/db';
 connectDB();
+import { notFound, errorHandler } from './middlewares/errorMiddleware';
 // App
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
